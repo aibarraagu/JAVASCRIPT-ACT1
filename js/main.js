0a6a2604277dd3629430ac119b894b747e3a0f41
aibarraagu <aibarraagu1@educacion.navarra.es>
2026-01-28 14:05:34 +0100

    git commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let numero = 10;
+function operacion() {
+    if (numero >= 10) {
+        console.log("caro de cojones");
+    } else {
+        console.log("baratongo entonces a pillar");
+    }
+
+}
+
+
+
+function segundaact() { 
+    let numero = 5;
+    if(numero );
+}
